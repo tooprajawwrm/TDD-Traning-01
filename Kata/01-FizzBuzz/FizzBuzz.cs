b541dcc4b0ea257d5130f6da0c4291366b57c6b2
Keerati <tooprajaw@gmail.com>
2022-03-28 15:32:17 +0700

    Green : Implement production code for Buzz

--- Kata/01-FizzBuzz/FizzBuzz.cs
+++ Kata/01-FizzBuzz/FizzBuzz.cs
@@ -4,6 +4,10 @@ public class FizzBuzz
 {
     public string GetFizzBuzz(int number)
     {
+        if(number == 5)
+        {
+            return "Buzz";
+        }
         if (number % 3 == 0)
         {
             return "Fizz";
